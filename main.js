829352a20eae7961759f149560ce1849e2cd9337
Luke Watermeyer <luke@watermeyer.tech>
2023-07-12 00:02:56 +0200

    Added ChunkRule Class and methods for displaying chunked stories

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ class Story {
     relHeight;
 
     chunk;
-    chunkRules;
+    chunkRule;
+
+    pages = null;
 
     constructor(options) {
         if (options.remote !== false) {
@@ -30,18 +32,57 @@ class Story {
 
             if (options.chunkText !== false) {
                 this.chunk = true;
-                this.chunkRules = {chunkChar: options.chunkText.char}
+                this.chunkRule = new ChunkRule(options.chunkText);
             }
         }
     }
 
     logSelf() {
-        console.log(`[Width: ${this.relWidth}, Height: ${this.relHeight}] chunkRules: ${JSON.stringify(this.chunkRules)}, contentLength: ${this.textContent.length}`);
+        console.log(`[Width: ${this.relWidth}, Height: ${this.relHeight}] chunkRule: ${this.chunkRule}, contentLength: ${this.textContent.length}`);
     }
 
     // The troubles with logging the chunkRules options shows EXACTLY why this whole thing whould be Object Oriented :)
+
+    start() {
+        let displayDiv = document.createElement("div");
+        displayDiv.id = 'display-div';
+        displayDiv.style.height = this.relHeight + 'vh';
+        displayDiv.style.width = this.relWidth + 'vw';
+        displayDiv.style.background = 'gray';
+        displayDiv.style.position = 'absolute';
+        displayDiv.style.bottom = '-' + this.relHeight + 'vh';
+        displayDiv.style.transition = 'bottom 1s';
+
+        document.body.appendChild(displayDiv);
+        displayDiv.style.bottom = '50vh';
+    }
+
+    next() {
+        if (this.pages === null) {
+            console.error('Error in "next()" function, Story has not been started yet!');
+        } else {
+
+        }
+    }
+
+    abort() {
+        if (this.pages === null) {
+            console.error('Error in "abort()" function, Story has not been started yet!');
+        } else {
+
+        }
+    }
 }
 
 class ChunkRule {
     chunkChar;
+
+    constructor(options) {
+        this.chunkChar = options.char;
+    }
+
+    toString() {
+        //JSON.stringify will escape hidden chars for us :)
+        return '{chunkChar: "' + JSON.stringify(this.chunkChar) + '", chunkCharCode: ' + this.chunkChar.charCodeAt() + '}';
+    }
 }
