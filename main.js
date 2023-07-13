3ed96d79ecb14ba03de3810772c76f0b87eaa044
Luke Watermeyer <luke@watermeyer.tech>
2023-07-13 11:49:32 +0200

    Fixed pagination and added conceal function for story's end

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ class Story {
 
     pages = null;
 
+    display;
+
     constructor(options) {
         if (options.remote !== false) {
 
@@ -44,24 +46,16 @@ class Story {
     // The troubles with logging the chunkRules options shows EXACTLY why this whole thing whould be Object Oriented :)
 
     start() {
-        let displayDiv = document.createElement("div");
-        displayDiv.id = 'display-div';
-        displayDiv.style.height = this.relHeight + 'vh';
-        displayDiv.style.width = this.relWidth + 'vw';
-        displayDiv.style.background = 'gray';
-        displayDiv.style.position = 'absolute';
-        displayDiv.style.bottom = '-' + this.relHeight + 'vh';
-        displayDiv.style.transition = 'bottom 1s';
-
-        document.body.appendChild(displayDiv);
-        displayDiv.style.bottom = '50vh';
+        this.pages = this.chunkRule.paginate(this.textContent);
+        this.display = new Display(this.relHeight, this.relWidth, this.pages);
+        this.display.reveal();
     }
 
     next() {
         if (this.pages === null) {
             console.error('Error in "next()" function, Story has not been started yet!');
         } else {
-
+            this.display.next();
         }
     }
 
@@ -74,8 +68,66 @@ class Story {
     }
 }
 
+class Display {
+    displayDiv;
+    
+    width;
+    height;
+    pages;
+
+    pageNo;
+
+    constructor(height, width, pages) {
+        this.width = width;
+        this.height = height;
+        this.pages = pages;
+
+        let left = (100 - this.width) / 2;
+
+        this.displayDiv = document.createElement("div");
+        this.displayDiv.id = 'display-div';
+        this.displayDiv.style.height = height + 'vh';
+        this.displayDiv.style.width = width + 'vw';
+        this.displayDiv.style.background = 'gray';
+        this.displayDiv.style.position = 'absolute';
+        this.displayDiv.style.left = left + 'vw';
+        this.displayDiv.style.bottom = '-' + height + 'vh';
+        this.displayDiv.style.transition = 'bottom 1s';
+
+        this.pageNo = 1;
+        this.displayDiv.innerHTML = this.pages[0];
+    }
+
+    reveal() {
+        document.body.appendChild(this.displayDiv);
+
+        setTimeout(function() {
+            let bottom = (100 - this.height) / 2;
+            this.displayDiv.style.bottom = bottom + 'vh';
+        }.bind(this), 500);
+    }
+
+    conceal() {
+        this.displayDiv.addEventListener('transitionend', function() {
+            this.displayDiv.remove();
+        }.bind(this));
+        this.displayDiv.style.bottom = '-' + this.height + 'vh';
+    }
+
+    next() {
+        if (this.pageNo === this.pages.length) {
+            this.conceal();
+        } else {
+            this.displayDiv.innerHTML = this.pages[this.pageNo++];
+        }
+    }
+}
+
 class ChunkRule {
     chunkChar;
+
+    // We can add in any chunking rules we want to support into this class
+    // e.g. preserveChunkChar, replacementChar
 
     constructor(options) {
         this.chunkChar = options.char;
@@ -84,5 +136,9 @@ class ChunkRule {
     toString() {
         //JSON.stringify will escape hidden chars for us :)
         return '{chunkChar: "' + JSON.stringify(this.chunkChar) + '", chunkCharCode: ' + this.chunkChar.charCodeAt() + '}';
+    }
+
+    paginate(text) {
+        return text.split(this.chunkChar);
     }
 }
